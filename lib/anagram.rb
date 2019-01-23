cf4f9df41b989e2a6d2c71eb9a99cacc934c046c
Michael Soares <mikey.soares@gmail.com>
2019-01-23 17:08:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,21 +9,21 @@ class Anagram
 
   def match(anagrams)
 
-  arr = []
-    anagrams.each do |i|
+  # arr = []
+  #   anagrams.each do |i|
     #
     binding.pry
-      split = i.split("").sort
-      word_split = self.word.split("").sort
-      if split == word_split
-        arr << i
-      else
-        arr = []
-    #   if i == @word
-    #     true
-      end
-    end
-    arr
+    #   split = i.split("").sort
+    #   word_split = self.word.split("").sort
+    #   if split == word_split
+    #     arr << i
+    #   else
+    #     arr = []
+    # #   if i == @word
+    # #     true
+    #   end
+    # end
+    # arr
   end
 
 
